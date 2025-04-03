2481a26fca06e4d0ce4016e59a170574024e8357
Christopher Lawrence <ChrstphrJLawrence@gmail.com>
2025-04-02 20:19:59 -0700

    add a component test

--- app/components/TableCard.test.tsx
+++ app/components/TableCard.test.tsx
@@ -16,4 +16,27 @@ describe("TableCard", () => {
         expect(screen.getByText("Available")).toBeInTheDocument()
         expect(screen.getByText("No session in progress")).toBeInTheDocument()
     })
+    it("renders in use when the table is in use", () => {
+        render(<TableCard table={{
+            id: 1,
+            name: "Table 1",
+            inUse: true,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            sessions: [
+                {
+                    id: 1,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    tableId: 1,
+                    startedAt: new Date(),
+                    endedAt: null,
+                    createdById: null,
+                    formattedStartedAt: "12:00",
+                }
+            ]
+        }} />)
+        expect(screen.getByText("In Use")).toBeInTheDocument()
+        expect(screen.getByText("Mark Available")).toBeInTheDocument()
+    })
 })
