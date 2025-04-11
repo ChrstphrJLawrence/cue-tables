05a257df12bfa6153fc0b6d5bb6df7033c4de04c
Christopher Lawrence <ChrstphrJLawrence@gmail.com>
2025-04-11 02:51:35 -0700

    on conflict fixes

--- prisma/migrations/20250411091914_seed_prod/migration.sql
+++ prisma/migrations/20250411091914_seed_prod/migration.sql
@@ -4,25 +4,31 @@ INSERT INTO UserImage VALUES('cm9cj99od0003aow0luebgss9',NULL,'user/kody.png',17
 
 INSERT INTO Password VALUES('$2b$10$8ZQqDuhJX0PgC08HYGgh/eEp2xwTjt2R.IjO2nd5qMtfEN.ZN1Beu','cm9cj99o30000aow03581bpgd');
 
-INSERT INTO Permission VALUES('clnf2zvli0000pcou3zzzzome','create','user','own','',1696625465526,1696625465526);
-INSERT INTO Permission VALUES('clnf2zvll0001pcouly1310ku','create','user','any','',1696625465529,1696625465529);
-INSERT INTO Permission VALUES('clnf2zvll0002pcouka7348re','read','user','own','',1696625465530,1696625465530);
-INSERT INTO Permission VALUES('clnf2zvlm0003pcouea4dee51','read','user','any','',1696625465530,1696625465530);
-INSERT INTO Permission VALUES('clnf2zvlm0004pcou2guvolx5','update','user','own','',1696625465531,1696625465531);
-INSERT INTO Permission VALUES('clnf2zvln0005pcoun78ps5ap','update','user','any','',1696625465531,1696625465531);
-INSERT INTO Permission VALUES('clnf2zvlo0006pcouyoptc5jp','delete','user','own','',1696625465532,1696625465532);
-INSERT INTO Permission VALUES('clnf2zvlo0007pcouw1yzoyam','delete','user','any','',1696625465533,1696625465533);
-INSERT INTO Permission VALUES('clnf2zvlp0008pcou9r0fhbm8','create','note','own','',1696625465533,1696625465533);
-INSERT INTO Permission VALUES('clnf2zvlp0009pcouj3qib9q9','create','note','any','',1696625465534,1696625465534);
-INSERT INTO Permission VALUES('clnf2zvlq000apcouxnspejs9','read','note','own','',1696625465535,1696625465535);
-INSERT INTO Permission VALUES('clnf2zvlr000bpcouf4cg3x72','read','note','any','',1696625465535,1696625465535);
-INSERT INTO Permission VALUES('clnf2zvlr000cpcouy1vp6oeg','update','note','own','',1696625465536,1696625465536);
-INSERT INTO Permission VALUES('clnf2zvls000dpcouvzwjjzrq','update','note','any','',1696625465536,1696625465536);
-INSERT INTO Permission VALUES('clnf2zvls000epcou4ts5ui8f','delete','note','own','',1696625465537,1696625465537);
-INSERT INTO Permission VALUES('clnf2zvlt000fpcouk29jbmxn','delete','note','any','',1696625465538,1696625465538);
+INSERT INTO Permission (id, action, entity, access, description, createdAt, updatedAt)
+VALUES 
+  ('clnf2zvli0000pcou3zzzzome','create','user','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvll0001pcouly1310ku','create','user','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvll0002pcouka7348re','read','user','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlm0003pcouea4dee51','read','user','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlm0004pcou2guvolx5','update','user','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvln0005pcoun78ps5ap','update','user','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlo0006pcouyoptc5jp','delete','user','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlo0007pcouw1yzoyam','delete','user','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlp0008pcou9r0fhbm8','create','note','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlp0009pcouj3qib9q9','create','note','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlq000apcouxnspejs9','read','note','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlr000bpcouf4cg3x72','read','note','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlr000cpcouy1vp6oeg','update','note','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvls000dpcouvzwjjzrq','update','note','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvls000epcou4ts5ui8f','delete','note','own','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlt000fpcouk29jbmxn','delete','note','any','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+ON CONFLICT(action, entity, access) DO NOTHING;
 
-INSERT INTO Role VALUES('clnf2zvlw000gpcour6dyyuh6','admin','',1696625465540,1696625465540);
-INSERT INTO Role VALUES('clnf2zvlx000hpcou5dfrbegs','user','',1696625465542,1696625465542);
+INSERT INTO Role (id, name, description, createdAt, updatedAt)
+VALUES 
+  ('clnf2zvlw000gpcour6dyyuh6','admin','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP),
+  ('clnf2zvlx000hpcou5dfrbegs','user','',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+ON CONFLICT(name) DO NOTHING;
 
 INSERT INTO Connection VALUES('cm9cj99o30001aow0i88yvqms','github','6483432875906615',1744360590387,1744360590387,'cm9cj99o30000aow03581bpgd');
 
@@ -46,10 +52,10 @@ INSERT INTO _PermissionToRole VALUES('clnf2zvls000epcou4ts5ui8f','clnf2zvlx000hp
 INSERT INTO _RoleToUser VALUES('clnf2zvlw000gpcour6dyyuh6','cm9cj99o30000aow03581bpgd');
 INSERT INTO _RoleToUser VALUES('clnf2zvlx000hpcou5dfrbegs','cm9cj99o30000aow03581bpgd');
 
-INSERT INTO "Table" VALUES(1,'Table 1',0,1744360590400,1744360590400);
-INSERT INTO "Table" VALUES(2,'Table 2',0,1744360590400,1744360590400);
-INSERT INTO "Table" VALUES(3,'Table 3',0,1744360590400,1744360590400);
-INSERT INTO "Table" VALUES(4,'Table 4',0,1744360590400,1744360590400);
-INSERT INTO "Table" VALUES(5,'Table 5',0,1744360590400,1744360590400);
-INSERT INTO "Table" VALUES(6,'Table 6',0,1744360590400,1744360590400);
+INSERT INTO "Table" VALUES(1,'Table 1',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO "Table" VALUES(2,'Table 2',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO "Table" VALUES(3,'Table 3',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO "Table" VALUES(4,'Table 4',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO "Table" VALUES(5,'Table 5',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO "Table" VALUES(6,'Table 6',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 
