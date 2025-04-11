1e4efb3368f64315e3e576c012edda1d46e24aee
Christopher Lawrence <ChrstphrJLawrence@gmail.com>
2025-04-11 03:02:19 -0700

    more conflict fixes

--- prisma/migrations/20250411091914_seed_prod/migration.sql
+++ prisma/migrations/20250411091914_seed_prod/migration.sql
@@ -32,25 +32,31 @@ ON CONFLICT(name) DO NOTHING;
 
 INSERT INTO Connection VALUES('cm9cj99o30001aow0i88yvqms','github','6483432875906615',1744360590387,1744360590387,'cm9cj99o30000aow03581bpgd');
 
-INSERT INTO _PermissionToRole VALUES('clnf2zvll0001pcouly1310ku','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlm0003pcouea4dee51','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvln0005pcoun78ps5ap','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlo0007pcouw1yzoyam','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlp0009pcouj3qib9q9','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlr000bpcouf4cg3x72','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvls000dpcouvzwjjzrq','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlt000fpcouk29jbmxn','clnf2zvlw000gpcour6dyyuh6');
-INSERT INTO _PermissionToRole VALUES('clnf2zvli0000pcou3zzzzome','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvll0002pcouka7348re','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlm0004pcou2guvolx5','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlo0006pcouyoptc5jp','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlp0008pcou9r0fhbm8','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlq000apcouxnspejs9','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvlr000cpcouy1vp6oeg','clnf2zvlx000hpcou5dfrbegs');
-INSERT INTO _PermissionToRole VALUES('clnf2zvls000epcou4ts5ui8f','clnf2zvlx000hpcou5dfrbegs');
+INSERT INTO _PermissionToRole (A, B)
+VALUES
+  ('clnf2zvll0001pcouly1310ku','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvlm0003pcouea4dee51','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvln0005pcoun78ps5ap','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvlo0007pcouw1yzoyam','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvlp0009pcouj3qib9q9','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvlr000bpcouf4cg3x72','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvls000dpcouvzwjjzrq','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvlt000fpcouk29jbmxn','clnf2zvlw000gpcour6dyyuh6'),
+  ('clnf2zvli0000pcou3zzzzome','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvll0002pcouka7348re','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvlm0004pcou2guvolx5','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvlo0006pcouyoptc5jp','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvlp0008pcou9r0fhbm8','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvlq000apcouxnspejs9','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvlr000cpcouy1vp6oeg','clnf2zvlx000hpcou5dfrbegs'),
+  ('clnf2zvls000epcou4ts5ui8f','clnf2zvlx000hpcou5dfrbegs')
+ON CONFLICT(A, B) DO NOTHING;
 
-INSERT INTO _RoleToUser VALUES('clnf2zvlw000gpcour6dyyuh6','cm9cj99o30000aow03581bpgd');
-INSERT INTO _RoleToUser VALUES('clnf2zvlx000hpcou5dfrbegs','cm9cj99o30000aow03581bpgd');
+INSERT INTO _RoleToUser (A, B)
+VALUES
+  ('clnf2zvlw000gpcour6dyyuh6','cm9cj99o30000aow03581bpgd'),
+  ('clnf2zvlx000hpcou5dfrbegs','cm9cj99o30000aow03581bpgd')
+ON CONFLICT(A, B) DO NOTHING;
 
 INSERT INTO "Table" VALUES(1,'Table 1',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
 INSERT INTO "Table" VALUES(2,'Table 2',0,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
